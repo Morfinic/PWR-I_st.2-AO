a0ac7a0bdf98b7527f1f51982f1fa693fafe9150
Filip Juchnowicz <morfinic@proton.me>
2025-03-25 16:47:49 +0100

    Szkielet VRP

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# First init test
+import json
+import os
+
+for file in os.listdir("./data"):
+    with open("./data/" + file) as f:
+        json_file = json.load(f)
+        print(json_file["data"])
