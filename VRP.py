a74baebfdb9c922bfe7f35d4d904f70392040f8c
Filip Juchnowicz <morfinic@proton.me>
2025-04-22 15:26:42 +0200

    Dodano działający? alogrytm tabu

--- VRP.py
+++ VRP.py
@@ -32,7 +32,8 @@ class VRP:
 
         client_id = 0
         for i in range(self.num_vehicles):
-            extra_client = 1 if free_client < i else 0
+            extra_client = 1 if free_client > 0 else 0
+            free_client -= 1
 
             solutions[i] = clients[client_id:client_id + clients_for_vehicle + extra_client]
             client_id += clients_for_vehicle + extra_client
@@ -53,15 +54,79 @@ class VRP:
                 total_distance += self._calculate_distance(self.locations[route[i]], self.locations[route[i + 1]])
 
             # Last client to depot
-            total_distance += self._calculate_distance(self.locations[route[-1]], self.locations[route[0]])
+            total_distance += self._calculate_distance(self.locations[route[-1]], self.locations[0])
 
-        return total_distance
+        # return total_distance
 
-    # TODO dodać losowość np. zamiana klientów między trasami, przerzucanie klientów na inną trasę
+        # Test kary za nierówne rozłożenie klientów
+        # Nie wiem niby działa, ale czy to nie forcuje że nigdy nie przerzuci klienta na inny pojazd?
+        # Można usunąć karę i wywalić przerzucanie na inną trasę i wyjdzie to samo
+        clients_per_vehicle = self.num_clients / self.num_vehicles
+        penalty = sum(abs(len(route) - clients_per_vehicle) for route in solutions) * 60
+        return total_distance + penalty
+
     def _update_route(self, solutions):
-        pass
+        possible_moves = ["swap_client_order"]
+        if self.num_vehicles > 1:
+            possible_moves += ["swap_between_couriers", "change_courier"]
+        move_type = random.choice(possible_moves)
+        move = None
 
-    def tabu_search(self, max_iter):
+        new_sol = copy.deepcopy(solutions)
+
+        if move_type == "swap_client_order":
+            valid_couriers = [i for i, route in enumerate(new_sol) if len(route) >= 2]
+            courier = random.choice(valid_couriers)
+
+            c1, c2 = random.sample(range(len(new_sol[courier])), 2)
+
+            new_sol[courier][c1], new_sol[courier][c2] = new_sol[courier][c2], new_sol[courier][c1]
+            move = ("swap_client_order", courier, new_sol[courier][c1], new_sol[courier][c2])
+        else:
+            valid_couriers = [i for i, route in enumerate(new_sol) if len(route) > 0]
+            if len(valid_couriers) < 2:
+                return self._update_route(solutions)
+
+            if move_type == "swap_between_couriers":
+                courier1 = random.choice(valid_couriers)
+
+                courier2 = random.choice([
+                    x
+                    for x in valid_couriers
+                    if x != courier1
+                ])
+
+                c1 = random.randint(0, len(new_sol[courier1]) - 1)
+                c2 = random.randint(0, len(new_sol[courier2]) - 1)
+
+                new_sol[courier1][c1], new_sol[courier2][c2] = new_sol[courier2][c2], new_sol[courier1][c1]
+
+                move = ("swap_between_couriers", courier1, courier2, new_sol[courier1][c1], new_sol[courier2][c2])
+            elif move_type == "change_courier":
+                courier1 = random.choice(valid_couriers)
+                courier2 = random.choice([
+                    x
+                    for x in valid_couriers
+                    if x != courier1
+                ])
+
+                client_id = random.randint(0, len(new_sol[courier1]) - 1)
+                client = new_sol[courier1].pop(client_id)
+                insert_pos = random.randint(0, len(new_sol[courier2]))
+                new_sol[courier2].insert(insert_pos, client)
+
+                move = ("change_courier", courier1, client, courier2, insert_pos)
+
+        # Plaster na kod żeby nie zrzucał wszystko na jeden pojazd
+        # Nie działa, poprostu zostawi minimum jeden na każdym pojeździe
+        if self.num_vehicles > 1:
+            for route in new_sol:
+                if len(route) == 0:
+                    return self._update_route(solutions)
+
+        return new_sol, move
+
+    def tabu_search(self, max_iter=100):
         current_solution = self._init_solution()
         current_cost = self._calculate_total_route_distance(current_solution)
 
@@ -69,4 +134,26 @@ class VRP:
         best_cost = current_cost
 
         for _ in range(max_iter):
-            pass
+            best_change = None
+            best_change_cost = float('inf')
+            best_move = None
+
+            for _ in range(10):
+                new_sol, move = self._update_route(current_solution)
+                new_cost = self._calculate_total_route_distance(new_sol)
+
+                if not self._is_tabu(move) or new_cost < best_cost:
+                    if new_cost < best_change_cost:
+                        best_change = new_sol
+                        best_change_cost = new_cost
+                        best_move = move
+
+            current_solution = best_change
+            current_cost = best_change_cost
+            self._update_tabu(best_move)
+
+            if current_cost < best_cost:
+                best_sol = copy.deepcopy(current_solution)
+                best_cost = current_cost
+
+        return [[0] + sol + [0] for sol in best_sol], best_cost
--- main.py
+++ main.py
@@ -3,24 +3,62 @@ import os
 from matplotlib import pyplot as plt
 from VRP import VRP
 
+# Losowe kolory, żeby wykres był ładny i jako tako czytelny
+colors = [
+    "#5e49b8",
+    "#73d464",
+    "#b34bd6",
+    "#cad04b",
+    "#c34a95",
+    "#5c7d3c",
+    "#d34844",
+    "#78cabc",
+    "#472c56",
+    "#c9863d",
+    "#718ec6",
+    "#7a3d35",
+    "#cabe92",
+    "#425146",
+    "#cb98b5"
+]
+
 for file in os.listdir("./data"):
     with open("./data/" + file) as f:
         json_file = json.load(f)
 
+        num_veh = 3
         vrp_problem: VRP = VRP(
             json_file["data"],
-            2
+            num_veh,
+            5
         )
 
-        vrp_problem.init_solution()
+        best_sol, best_cost = vrp_problem.tabu_search(1500)
 
-        pass
+        print(best_sol)
+        print(best_cost)
 
-        # # Scatter plot na pokazanie punktów
-        # # annotate aby pokazać trasę pomiędzy punktami
-        # plt.scatter(*zip(*json_file["data"]))
+        # Scatter plot na pokazanie punktów
+        # annotate aby pokazać trasę pomiędzy punktami
+
+        # Depot
+        plt.scatter(*json_file["data"][0], color="red", s=200)
+        # Klienci
+        plt.scatter(*zip(*json_file["data"][1:]), s=200)
+
+        for txt, coords in enumerate(json_file["data"]):
+            plt.text(*coords, txt, ha="center", va="center", color="white")
+
+        for i in range(len(best_sol)):
+            color = colors[i]
+            for j in range(len(best_sol[i]) - 1):
+                plt.annotate(
+                    "", xytext=json_file["data"][best_sol[i][j]], xy=json_file["data"][best_sol[i][j + 1]],
+                    arrowprops=dict(arrowstyle="->,head_length=1,head_width=0.5", color=color)
+                )
         # plt.annotate(
         #     "", xytext=json_file["data"][0], xy=json_file["data"][1],
         #     arrowprops=dict(arrowstyle="->", color="red")
         # )
-        # plt.show()
+
+        plt.show()
