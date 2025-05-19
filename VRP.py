6ab925cbf1b2f0e69630eae261ffe9e1886f0929
wpap <mrwocior@gmail.com>
2025-05-19 14:32:40 +0200

    added benchmark loop with seed support

--- VRP.py
+++ VRP.py
@@ -4,12 +4,16 @@ import numpy as np
 
 
 class VRP:
-    def __init__(self, locations: list[list], num_vehicles: int, tabu_limit: int = 3) -> None:
+    def __init__(self, locations: list[list], num_vehicles: int, tabu_limit: int = 3, seed: int = None) -> None:
         self.locations = locations
         self.num_vehicles = num_vehicles
         self.num_clients = len(locations) - 1
         self.tabu_list = []
         self.tabu_limit = tabu_limit
+        
+        if seed is not None:
+            random.seed(seed)
+            np.random.seed(seed)
 
     def _calculate_distance(self, location1: list, location2: list) -> float:
         return np.sqrt(np.pow(location1[0] - location2[0], 2) + np.pow(location1[1] - location2[1], 2))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from VRP import VRP
 import xml.etree.ElementTree as ET
 from or_tools import or_tools_solve
+import numpy as np
 
 def plot_sol(data, best_sol):
     # Depot
@@ -43,7 +44,7 @@ colors = [
     "#cb98b5"
 ]
 
-for file in os.listdir("./data"):
+for file in os.listdir("./data_bench"): # zmiana katologu pod testowanie petli
     print(f"\nReading {file}")
 
     dataset = {
@@ -58,7 +59,7 @@ for file in os.listdir("./data"):
         dataset["num_of_vehicles"] = json_file["num_of_vehicles"]
 
     elif file.endswith(".xml"):
-        tree = ET.parse("./data/" + file)
+        tree = ET.parse("./data_bench/" + file) # zmiana katologu pod testowanie petli
         root = tree.getroot()
 
         for node in root.findall("network/nodes/node"):
@@ -69,19 +70,58 @@ for file in os.listdir("./data"):
 
         for node in root.findall("fleet/vehicle_profile"):
             dataset["num_of_vehicles"] += 1
-
+    
+    """
+    # Tutaj dałem wcześniejsze wywoływanie VRP przed benchmarkiem
+    
     vrp_problem: VRP = VRP(
-        dataset["data"],
-        dataset["num_of_vehicles"],
-        5
-    )
-
+                dataset["data"],
+                dataset["num_of_vehicles"],
+                5,
+                seed
+            )
     # tabu search
     best_sol, best_cost = vrp_problem.tabu_search(2000)
     print(f"Tabu:\n{best_sol}\n{best_cost}")
     plot_sol(dataset, best_sol)
-
-    # or-tools
+    # or-tools        
     best_sol, best_cost = or_tools_solve(dataset)
     print(f"Or-Tools:{best_sol}\n{best_cost}")
     plot_sol(dataset, best_sol)
+    """
+    
+    max_iters = [250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500]
+    results = []
+    np.random.seed(42)
+    
+    for it in max_iters:
+        costs = []
+        for id in range(10):
+            seed = np.random.randint(0, 1e6)
+            vrp_problem: VRP = VRP(
+                dataset["data"],
+                dataset["num_of_vehicles"],
+                5,
+                seed
+            )
+            best_sol, best_cost = vrp_problem.tabu_search(it)
+            costs.append(best_cost)
+            results.append({
+                "algorithm": "Tabu",
+                "run_id": id,
+                "seed": seed,
+                "max_iter": it,
+                "cost": best_cost
+            })
+        avg_cost = np.mean(costs)
+        print(f"Tabu\t{it}\t{avg_cost:.2f}")
+    
+    # or-tools
+    best_sol, best_cost = or_tools_solve(dataset)
+    print(f"Or-Tools:{best_sol}\n{best_cost}")
+    results.append({
+        "algorithm": "or-tools",
+        "run_id": 0,
+        "seed": "-",
+        "cost": best_cost
+    })
